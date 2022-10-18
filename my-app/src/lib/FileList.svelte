340a593f06d362cf24367db6a7e3bbed8c750c95
Sean-Shmulevich <zecoolioguy@gmail.com>
2022-10-17 20:53:46 -0400

    Everything is pushed now

---
+++ my-app/src/lib/FileList.svelte
@@ -0,0 +1,134 @@
+<script context="module">
+    import DragDropTouch from 'svelte-drag-drop-touch'
+    import {asDraggable} from 'svelte-drag-and-drop-actions'
+</script>
+<script>
+    import '../css/98.css';
+    import '../css/myStyle.css';
+    import {createEventDispatcher} from 'svelte';
+
+    import {count} from '../stores/zIndex.js';
+
+    const dispatch = createEventDispatcher();
+
+    function forward(event) {
+        dispatch('close', event.detail);
+    }
+
+    export let zIdx = 0;
+    let BoxX = 200, BoxY = 200;//starting coords
+    function onDragStart() {
+        return {x: BoxX, y: BoxY}
+    }
+
+    function onDragMove(x, y, dx, dy) {
+        BoxX = x;
+        BoxY = y
+    }
+
+    function onDragEnd(x, y, dx, dy) {
+        BoxX = x;
+        BoxY = y
+    }
+
+
+    function incrementCount() {
+        if (zIdx > $count) {
+            count.set(zIdx);
+            console.log($count);
+        } else if (zIdx == $count) {
+            zIdx += 2;
+            count.set(zIdx);
+        } else {
+            zIdx = $count + 1;
+            count.set(zIdx);
+        }
+    }
+
+</script>
+<div class="linksBox" id="windowOutter" style="
+         position:fixed;
+         left:{BoxX}px;
+         top:{BoxY}px;
+         cursor:move;
+         z-index: {zIdx};" on:mousedown={incrementCount}>
+
+
+    <div class="title-bar linksBar windowBar"
+         use:asDraggable={{relativeTo:document.body, onDragStart, onDragMove, onDragEnd, minX:0,minY:0}}>
+        <div class="title-bar-text"
+             style="text-align:right;float:left;font-size: 10px;margin-left: 10px;margin-top: 4px;">A Window With A
+            Status Bar
+        </div>
+        <div class="title-bar-controls" style="float: right;margin-right: 5px;padding-top: 5px;">
+            <button class="minimize" style="min-width: 15px;background-color: #ff2121;" aria-label="Minimize"></button>
+            <button class="full" style="min-width: 15px;margin-left: 2px;background-color: #ffcf21;"
+                    aria-label="Maximize"></button>
+            <button class="close" on:mousedown={forward}
+                    style="min-width: 15px;background-color: #21ff3f;" aria-label="Close"></button>
+        </div>
+    </div>
+
+    <div style="border: 1px solid #000000;display: inline-block;width: inherit;">
+
+        <fieldset class="fileMenuTop" style="margin-bottom: 0px;">
+            <div class="status-bar">
+                <p class="status-bar-field">Press F1 for help</p>
+                <p class="status-bar-field">Slide 1</p>
+                <p class="status-bar-field">CPU Usage: 14%</p>
+            </div>
+        </fieldset>
+
+
+        <div class="window myWindow linksWindow">
+
+
+            <hr>
+            <div class="window-body">
+
+                <div class="fileMenuItem">
+                    <img id="fileIcon" src="https://cdn-icons-png.flaticon.com/512/124/124837.png"/>
+
+                    <p style="margin-left: 26px;">github</p>
+                    <p style="margin: auto;">3Kb</p>
+                    <p style="margin-left: 26px;">public</p>
+
+                </div>
+                <hr>
+
+                <div class="fileMenuItem">
+                    <img id="fileIcon" src="https://cdn-icons-png.flaticon.com/512/124/124837.png"/>
+
+                    <p style="margin-left: 26px;">LinkedIn</p>
+                    <p style="margin: auto;">3Kb</p>
+                    <p style="margin-left: 26px;">public</p>
+
+                </div>
+
+                <hr>
+
+
+                <div class="fileMenuItem">
+                    <img id="fileIcon" src="https://cdn-icons-png.flaticon.com/512/124/124837.png"/>
+
+                    <p style="margin-left: 26px;">Contact Info</p>
+                    <p style="margin: auto;">3Kb</p>
+                    <p style="margin-left: 26px;">public</p>
+
+                </div>
+
+
+            </div><!--window ends-->
+
+            <fieldset class="fileMenuBottom" style="margin-top: -2px;">
+                <div class="status-bar">
+                    <p class="status-bar-field">Press F1 for help</p>
+                    <p class="status-bar-field">Slide 1</p>
+                    <p class="status-bar-field">CPU Usage: 14%</p>
+                </div>
+            </fieldset>
+        </div>
+
+    </div>
+
+</div>
---
+++ my-app/src/lib/FilePage.svelte
@@ -0,0 +1,153 @@
+<script context="module">
+    import  DragDropTouch  from 'svelte-drag-drop-touch'
+    import { asDraggable } from 'svelte-drag-and-drop-actions'
+</script>
+<script>
+    import '../css/98.css';
+    import '../css/myStyle.css';
+    import {createEventDispatcher} from 'svelte';
+
+    import { count } from '../stores/zIndex.js';
+    export let zIdx = 0;
+
+    const dispatch = createEventDispatcher();
+
+    function forward(event) {
+        dispatch('close', event.detail);
+    }
+
+    let BoxX = 200, BoxY = 200;//starting coords
+    function onDragStart () { return { x:BoxX,y:BoxY} }
+    function onDragMove (x,y, dx,dy) { BoxX = x; BoxY = y }
+    function onDragEnd  (x,y, dx,dy) { BoxX = x; BoxY = y }
+
+    export const incrementCount = () => {
+        if(zIdx > $count){
+            count.set(zIdx);
+            console.log($count);
+        }
+        else if(zIdx == $count){
+            zIdx += 2;
+            count.set(zIdx);
+        }
+        else {
+            zIdx = $count + 1;
+            count.set(zIdx);
+        }
+        return zIdx;
+	};
+    
+</script>
+<div class="remBoxMobile" style="
+        position:fixed;
+        left:{BoxX}px; top:{BoxY}px;
+        z-index: {zIdx};
+    " on:mousedown={incrementCount}> 
+    <div class="title-bar fileGridBar windowBar" style="cursor:move" use:asDraggable={{relativeTo:document.body, onDragStart, onDragMove, onDragEnd, minX:0,minY:0}}>
+      <div class="title-bar-text" style="text-align:right;float:left;font-size: 10px;margin-left: 10px;margin-top: 4px;">A Window With A Status Bar</div>
+      <div class="title-bar-controls" style="position: relative;float: right;margin-right: 5px;padding-top: 5px;">
+        <button class="minimize" style="min-width: 15px;background-color: #ff2121;" aria-label="Minimize"></button>
+        <button class="full" style="min-width: 15px;margin-left: 2px;background-color: #ffcf21;" aria-label="Maximize"></button>
+        <button class="close" style="min-width: 15px;background-color: #21ff3f;" aria-label="Close" on:mousedown={forward}></button>
+      </div>
+    </div>
+  
+    <div style="border: 1px solid #000000;display: inline-block;width: 414px;margin-top: -6px">
+  
+      <fieldset class="fileMenuTop" style="margin-bottom: 0px;">
+        <div class="status-bar">
+          <p class="status-bar-field">Press F1 for help</p>
+          <p class="status-bar-field">Slide 1</p>
+          <p class="status-bar-field">CPU Usage: 14%</p>
+        </div>
+        </fieldset>
+      
+        
+          <div class="window myWindow" style="background: white;width: 400px;margin-left: 0px;border: 4px solid #c0c0c0!important;box-shadow: inset -1px -1px #000000, inset 1px 1px #0a0a0a, inset -2px -2px #808080, inset 2px 2px #dfdfdf;">
+            
+            
+  
+            <div class="window-body" >
+  
+                <div class="menuItem">
+                      <div class="exploreIcon">
+                        <img id="menuFileIcon" src="https://cdn-icons-png.flaticon.com/512/124/124837.png"/>
+                        <p id="filetext">hello</p>
+                      </div>
+                      
+                      <div class="exploreIcon">
+                        <img id="menuFileIcon" src="https://cdn-icons-png.flaticon.com/512/124/124837.png"/>
+                        <p id="filetext">hello</p>
+                      </div>
+                      <div class="exploreIcon">
+                        <img id="menuFileIcon" src="https://cdn-icons-png.flaticon.com/512/124/124837.png"/>
+                        <p id="filetext">hello</p>
+                      </div>
+                      <div class="exploreIcon">
+                        <img id="menuFileIcon" src="https://cdn-icons-png.flaticon.com/512/124/124837.png"/>
+                        <p id="filetext">hello</p>
+                      </div>
+                </div>
+  
+  
+        <div class="menuItem">
+          <div class="exploreIcon">
+            <img id="menuFileIcon" src="https://cdn-icons-png.flaticon.com/512/124/124837.png"/>
+            <p id="filetext">hello</p>
+          </div>
+          
+          <div class="exploreIcon">
+            <img id="menuFileIcon" src="https://cdn-icons-png.flaticon.com/512/124/124837.png"/>
+            <p id="filetext">hello</p>
+          </div>
+          <div class="exploreIcon">
+            <img id="menuFileIcon" src="https://cdn-icons-png.flaticon.com/512/124/124837.png"/>
+            <p id="filetext">hello</p>
+          </div>
+          <div class="exploreIcon">
+            <img id="menuFileIcon" src="https://cdn-icons-png.flaticon.com/512/124/124837.png"/>
+            <p id="filetext">hello</p>
+          </div>
+        </div>
+  
+  
+        <div class="menuItem">
+          <div class="exploreIcon">
+            <img id="menuFileIcon" src="https://cdn-icons-png.flaticon.com/512/124/124837.png"/>
+            <p id="filetext">hello</p>
+          </div>
+          
+          <div class="exploreIcon">
+            <img id="menuFileIcon" src="https://cdn-icons-png.flaticon.com/512/124/124837.png"/>
+            <p id="filetext">hello</p>
+          </div>
+          <div class="exploreIcon">
+            <img id="menuFileIcon" src="https://cdn-icons-png.flaticon.com/512/124/124837.png"/>
+            <p id="filetext">hello</p>
+          </div>
+          <div class="exploreIcon">
+            <img id="menuFileIcon" src="https://cdn-icons-png.flaticon.com/512/124/124837.png"/>
+            <p id="filetext">hello</p>
+          </div>
+  
+        </div>
+  
+        
+  
+  
+  
+            </div><!--window ends -->
+  
+            <fieldset class="fileMenuBottom" style="transform: translateY(-2px)">
+            <div class="status-bar">
+              <p class="status-bar-field">Press F1 for help</p>
+              <p class="status-bar-field">Slide 1</p>
+              <p class="status-bar-field">CPU Usage: 14%</p>
+            </div>
+            </fieldset>
+          </div>
+  
+  
+    </div>
+  </div>
+  
