d9a2cab17e850b433bf9f7d37938d045e1ec0962
Sean Shmulevich <seanshmulevich@seans-mbp-3.lan>
2022-12-08 15:55:05 -0500

    back to normal

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import autoPreprocess from 'svelte-preprocess';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base:"/personalWebsiteSvelte/",
   plugins: [svelte({
           preprocess: autoPreprocess()
         })]
