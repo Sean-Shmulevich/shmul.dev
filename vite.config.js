cfe207906fc455d6c843eb1c353c7c3c89213ae5
Sean Shmulevich <seanshmulevich@seans-mbp-3.lan>
2022-12-09 00:39:55 -0500

    temporary version

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,6 @@ import autoPreprocess from 'svelte-preprocess';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:"/personalWebsiteSvelte/",
   plugins: [svelte({
           preprocess: autoPreprocess()
         })]
