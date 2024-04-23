25761c7ca5f3e605730695617681f085419ae187
Sean Shmulevich <shmul@Ahava.local>
2024-04-23 18:51:15 -0400

    new base

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,13 @@
-import { defineConfig } from 'vite'
-import { svelte } from '@sveltejs/vite-plugin-svelte'
-import autoPreprocess from 'svelte-preprocess';
+import { defineConfig } from "vite";
+import { svelte } from "@sveltejs/vite-plugin-svelte";
+import autoPreprocess from "svelte-preprocess";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // base: "/personalWebsiteSvelte",
-  plugins: [svelte({
-    preprocess: autoPreprocess()
-  })]
-})
+    base: "/personalWebsiteSvelte",
+    plugins: [
+        svelte({
+            preprocess: autoPreprocess(),
+        }),
+    ],
+});
